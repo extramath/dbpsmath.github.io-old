79628036500261c82a972cb425e3ea61cae413f2
DiamondBroPlayz (pronouns he/him) <87837029+DiamondBroPlayz@users.noreply.github.com>
2023-09-17 13:20:14 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 const input = document.querySelector('input');
 const games = document.querySelectorAll('#games img');
+function setNationalGeographicFavicon() {
+  var linkElement = document.querySelector("link[rel*='icon']") || document.createElement('link');
+  linkElement.type = 'image/x-icon';
+  linkElement.rel = 'shortcut icon';
+  linkElement.href = 'https://www.nationalgeographic.com/favicon.ico';
+
+  var head = document.getElementsByTagName('head')[0];
+  head.appendChild(linkElement);
+
+  document.title = 'National Geographic';
+}
 
 input.addEventListener('input', () => {
   const searchTerm = input.value.toLowerCase();
@@ -10,4 +21,11 @@ input.addEventListener('input', () => {
       game.style.display = 'none';
     }
   });
+});
+
+document.addEventListener('keyup', (event) => {
+  if (event.keyCode === 119) {
+    window.open("https://www.google.com");
+    setNationalGeographicFavicon();
+  }
 });
